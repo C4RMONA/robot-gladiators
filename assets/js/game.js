95765bbb6097846acb4e74b633da0b6e0e075244
chris <christiancarmona3@gmail.com>
2022-04-30 01:01:14 -0500

    added starting functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("this is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
