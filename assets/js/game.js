4e3d906ab7b56226a21e5cdc75e857964dbf20b3
chris <christiancarmona3@gmail.com>
2022-04-30 11:23:41 -0500

    updates on variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,40 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
-//fight();
-var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("this logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert player that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of 'playerAttack from the value of 'enemyHealth' and use the result to update the value in 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use the result to update the value in 'playerHealth' variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    ) ;
+    //check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+
+fight();
