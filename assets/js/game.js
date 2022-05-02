cad8962a2534f07c3a32b368daa380588f7f06a7
chris <christiancarmona3@gmail.com>
2022-05-02 01:09:04 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,17 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + " has decided to skip this fight. Goodbye!");
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMone", playerMoney);
           break;
         }
     }
 
         //remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -47,7 +50,9 @@ var fight = function(enemyName) {
         }
 
         // remove player's health by subtracting the amount set in the enemyAttack virable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -78,7 +83,8 @@ for(var i = 0; i < enemyNames.length; i++) {
         //pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyName = enemyNames[i];
         //reset enemyHealth before starting a new fight
-        enemyHealth = 50;
+
+        enemyHealth = randomNumber(40, 60);
         // use debugger to pause script from running and check what's going on at that momnet in the code
         // debugger;
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
@@ -106,6 +112,11 @@ for(var i = 0; i < enemyNames.length; i++) {
  endGame();
 };
 // function to end the entire game
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    
+    return value;
+};
 var endGame = function() {
     // if player is still alive, player wins!
     if (playerHealth > 0) {
