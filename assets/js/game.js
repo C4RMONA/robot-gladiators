5048371de5cd34881bc170c66277b297b5608396
chris <christiancarmona3@gmail.com>
2022-05-08 16:45:42 -0500

    highscore feature added

--- assets/js/game.js
+++ assets/js/game.js
@@ -147,6 +147,22 @@ var endGame = function() {
   } else {
     window.alert("You've lost your robot in battle.");
     }
+    //check localStorage for high score, if it's not there use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+      highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+  
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+  
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
